c863a66af828e576cb46d42d2fc09abd76b405e0
Ewout Prangsma <ewout@prangsma.net>
2016-02-02 15:36:06 +0100

    Added environment defaults for vault-cacert & vault-capath

--- main.go
+++ main.go
@@ -46,11 +46,13 @@ var (
 )
 
 func init() {
+	globalFlags.VaultCACert = os.Getenv("VAULT_CACERT")
+	globalFlags.VaultCAPath = os.Getenv("VAULT_CAPATH")
 	cmdMain.PersistentFlags().BoolVarP(&globalFlags.debug, "debug", "D", false, "Print debug output")
 	cmdMain.PersistentFlags().BoolVarP(&globalFlags.verbose, "verbose", "v", false, "Print verbose output")
 	cmdMain.PersistentFlags().StringVar(&globalFlags.VaultAddr, "vault-addr", "", "URL of the vault (defaults to VAULT_ADDR environment variable)")
-	cmdMain.PersistentFlags().StringVar(&globalFlags.VaultCACert, "vault-cacert", "", " 	Path to a PEM-encoded CA cert file to use to verify the Vault server SSL certificate")
-	cmdMain.PersistentFlags().StringVar(&globalFlags.VaultCAPath, "vault-capath", "", "Path to a directory of PEM-encoded CA cert files to verify the Vault server SSL certificate")
+	cmdMain.PersistentFlags().StringVar(&globalFlags.VaultCACert, "vault-cacert", globalFlags.VaultCACert, "Path to a PEM-encoded CA cert file to use to verify the Vault server SSL certificate")
+	cmdMain.PersistentFlags().StringVar(&globalFlags.VaultCAPath, "vault-capath", globalFlags.VaultCAPath, "Path to a directory of PEM-encoded CA cert files to verify the Vault server SSL certificate")
 	cmdMain.PersistentFlags().StringVar(&globalFlags.TokenPath, "token-path", "", "Path of a file containing your vault token (token defaults to VAULT_TOKEN environment variable)")
 	cmdMain.PersistentFlags().StringVarP(&globalFlags.githubToken, "github-token", "G", "", "Personal github token for administrator logins")
 }
